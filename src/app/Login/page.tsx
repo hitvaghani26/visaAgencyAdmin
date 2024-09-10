2135f75381299080e34a75112f1d1e88d8897ca7
hitvaghani26 <vaghanihit123@gmail.com>
2024-09-10 20:41:29 +0530

    changes css in login and forgot pass

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -5,14 +5,13 @@ const page = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
 
-    <div className="  bg-[#F8F9FD] bg-gradient-to-t from-white to-[#F4F7FB] rounded-[40px] p-6 border-5  shadow-[rgba(224,36,84,0.4)_0px_10px_30px_1px,rgba(13,66,110,0.4)_0px_10px_30px_1px] mx-auto my-5
-">
+    <div className="  bg-[#F8F9FD] bg-gradient-to-t from-white to-[#F4F7FB] rounded- p-6 border-5  ">
     <div className="w-[500px] text-center font-black text-[30px] text-[#0D426E]">Log in</div>
 
     <form action="" className="mt-5">
       <input
         required
-        className="w-full bg-white  py-3 px-5 rounded-[20px] mt-4 shadow-[0px_10px_10px_-5px_#cff0ff]  border-2 border-transparent focus:border-[#E02454] focus:outline-none"
+        className="w-full bg-white  py-3 px-5 rounded-lg mt-4 shadow-[0px_10px_10px_-5px_#cff0ff]  border-2 border-transparent focus:border-[#E02454] focus:outline-none"
         type="email"
         name="email"
         id="email"
@@ -21,7 +20,7 @@ const page = () => {
 
       <input
         required
-        className="w-full bg-white  py-3 px-5 rounded-[20px] mt-4 shadow-[0px_10px_10px_-5px_#cff0ff] border-solid border-2 border-transparent focus:border-[#E02454] focus:outline-none"
+        className="w-full bg-white  py-3 px-5 rounded-lg mt-4 shadow-[0px_10px_10px_-5px_#cff0ff] border-solid border-2 border-transparent focus:border-[#E02454] focus:outline-none"
         type="password"
         name="password"
         id="password"
@@ -35,7 +34,7 @@ const page = () => {
       </span>
 
       <input
-        className="block w-full font-bold bg-gradient-to-br from-[#E02454] to-[#0D426E] text-white py-3 mt-5 rounded-[20px] shadow-[rgba(133,189,215,0.88)_0px_20px_10px_-15px] border-none transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-[rgba(133,189,215,0.88)_0px_23px_10px_-20px] active:scale-95 active:shadow-[rgba(133,189,215,0.88)_0px_15px_10px_-10px]"
+        className="block w-full font-bold bg-gradient-to-br from-[#E02454] to-[#0D426E] text-white py-3 mt-5 rounded-lg shadow-[rgba(133,189,215,0.88)_0px_20px_10px_-15px] border-none transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-[rgba(133,189,215,0.88)_0px_23px_10px_-20px] active:scale-95 active:shadow-[rgba(133,189,215,0.88)_0px_15px_10px_-10px]"
         type="submit"
         value="Sign In"
         />
